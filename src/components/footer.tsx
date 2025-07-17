d9d97c7921d7136da7b3aa5b4c309a6f01e6a7b7
gbmouraa <gabrielmoura-cma@hotmail.com>
2025-07-16 21:03:37 -0300

    Add social login options and footer to CreateAccount component; include Google, Microsoft, and phone login buttons

---
+++ src/components/footer.tsx
@@ -0,0 +1,29 @@
+export const Footer = () => {
+  return (
+    <footer className="mt-10 flex gap-x-2 text-sm text-gray-500 underline">
+      <a
+        href="https://www.linkedin.com/in/gabriel-moura-b63382161/"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        Linkedin
+      </a>
+      <span>|</span>
+      <a
+        href="https://github.com/gbmouraa"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        Github
+      </a>
+      <span>|</span>
+      <a
+        href="https://portfolio-gabrieldev.vercel.app/"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        Portfólio
+      </a>
+    </footer>
+  );
+};
--- src/pages/register/create-account.tsx
+++ src/pages/register/create-account.tsx
@@ -7,6 +7,10 @@ import { Link } from "react-router-dom";
 import { SiAuthelia } from "react-icons/si";
 import { Container } from "../../components/container";
 import { Input } from "../../components/input";
+import googleLogo from "../../assets/logo-google.svg";
+import microsoftLogo from "../../assets/logo-microsoft.svg";
+import phoneIcon from "../../assets/phone-icon.svg";
+import { Footer } from "../../components/footer";
 
 const schema = z.object({
   email: z.email("O email não válido").nonempty("Preencha seu email"),
@@ -28,26 +32,62 @@ export const CreateAccount = () => {
   };
 
   return (
-    <Container>
-      <Link to="/" className="mb-2 flex items-center gap-x-1 sm:hidden">
-        <SiAuthelia size={24} />
-        <span className="text-xl font-medium">Auth</span>
-      </Link>
-      <h1 className="text-3xl">Criar conta</h1>
-      <form className="mt-7 w-full" onSubmit={handleSubmit(onSubmit)}>
-        <fieldset>
-          <Input
-            type="text"
-            placeholder="Endereço de email"
-            name="email"
-            register={register}
-            error={errors.email?.message}
-          />
-        </fieldset>
-        <button className="mt-7 h-[50px] w-full cursor-pointer rounded-full bg-black text-white transition-opacity hover:opacity-80">
-          Continuar
-        </button>
-      </form>
-    </Container>
+    <>
+      <Container>
+        <Link to="/" className="mb-6 flex items-center gap-x-1 sm:hidden">
+          <SiAuthelia size={24} />
+          <span className="text-xl font-medium">Auth</span>
+        </Link>
+        <h1 className="text-3xl">Criar conta</h1>
+        <form className="mt-7 w-full" onSubmit={handleSubmit(onSubmit)}>
+          <fieldset>
+            <Input
+              type="text"
+              placeholder="Endereço de email"
+              name="email"
+              register={register}
+              error={errors.email?.message}
+            />
+          </fieldset>
+          <button className="mt-7 h-[50px] w-full cursor-pointer rounded-full bg-black text-white transition-opacity hover:opacity-80">
+            Continuar
+          </button>
+        </form>
+        <div className="mt-7 w-full">
+          <p className="text-center">
+            Já possui uma conta?{" "}
+            <Link to="/login" className="text-blue-600 hover:underline">
+              Entrar
+            </Link>
+          </p>
+          <div className="mt-6 flex items-center justify-center gap-x-2">
+            <div className="h-px w-full bg-gray-200"></div>
+            <span className="block text-center text-xs font-semibold tracking-wide">
+              OU
+            </span>
+            <div className="h-px w-full bg-gray-200"></div>
+          </div>
+          <div className="mt-6 space-y-3">
+            <button className="flex h-[50px] w-full cursor-pointer items-center gap-x-3 rounded-full border border-gray-300 px-5 transition-colors hover:bg-gray-200/60">
+              <img src={googleLogo} alt="Logo do Google" className="w-4" />
+              Continuar com Google
+            </button>
+            <button className="flex h-[50px] w-full cursor-pointer items-center gap-x-3 rounded-full border border-gray-300 px-5 transition-colors hover:bg-gray-200/60">
+              <img
+                src={microsoftLogo}
+                alt="Logo da Microsoft"
+                className="w-[19px]"
+              />
+              Continuar com Microsoft
+            </button>
+            <button className="flex h-[50px] w-full cursor-pointer items-center gap-x-3 rounded-full border border-gray-300 px-5 transition-colors hover:bg-gray-200/60">
+              <img src={phoneIcon} alt="Icone telefone" className="w-[20px]" />
+              Continuar com telefone
+            </button>
+          </div>
+        </div>
+      </Container>
+      <Footer />
+    </>
   );
 };
